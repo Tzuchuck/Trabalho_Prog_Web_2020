50a0d5afcd7dad9005b1d3f674c53207b3aefc70
Clara <clara.sanabria@outlook.com>
2020-10-08 20:51:52 -0400

    Add files via upload
    N sei o que to fazendo

---
+++ script (1).js
@@ -0,0 +1,7 @@
+function criar(){
+    window.location.replace("/Users/clara.sanabria/Documents/cod/st/favoritos.html");
+}
+
+function ir-fav){
+    window.location.replace("/Users/clara.sanabria/Documents/cod/st/favoritos.html");
+}
